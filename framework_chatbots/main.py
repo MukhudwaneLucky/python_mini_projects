81e585c9b2f6b8a92930db51efb130835ac14edb
MukhudwaneLucky <mukhudwane17@gmail.com>
2021-06-14 22:02:09 +0200

    added an abstract method for subclasses to implement their own mechanism for handling raw strings of data

--- framework_chatbots/main.py
+++ framework_chatbots/main.py
@@ -15,6 +15,13 @@ class Dialog(abc.ABC):
             the interpret method to determine the response. If a response
             is requested, the respond method is used to generate a text
             response based on the most recent input and the current state.
+            :param text     -> text as a string
+            :param response -> a boolean indicating if initiative has passed to
+                            the Dialog and a response is required. if False, Dialog
+                            listens and updates internal state
+            :param **kwargs   -> arbitrary keyword arguments which may contain other
+                            contextual info like user, session id or transcription score
+            :return a response if required, None if not, and a confidence score (0.0 - 1.0)
         """
         # parse the input
         sents = self.parse(text)
@@ -28,6 +35,18 @@ class Dialog(abc.ABC):
 
         # return initiative
         return reply, confidence
+
+    @abc.abstractmethod
+    def parse(self, text):
+        """
+            Every dialog may need its own parsing strategy, some dialogs may need
+            dependency vs constituency parses, others may simply require regular
+            expressions or chunkers.
+        :param text: a string as input
+        :return: a list of data structures specific to the needs of the particular
+                Dialog behavior
+        """
+        return []
 
 
 # run
