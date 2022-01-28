39fa59e3ab99aa4c8d9ff05938c98b2a181733eb
MukhudwaneLucky <mukhudwane17@gmail.com>
2022-01-29 01:00:00 +0200

    docs

---
+++ documentation/language_basics.py
@@ -0,0 +1,9 @@
+# This part describes the most commonly-used python statement.
+# Examples in this part refer to rules of various common casino games.
+
+# The print() function
+# Description : takes a list of values and prints their string representation on the terminal window.
+# Syntax : print expression < , ... >
+#            , 
+# Example Usage : print the value of PI
+print("PI = ", 355.0/113.0)
